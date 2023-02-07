c947b0b0beb8159a24f3033b2d6c2274f0a6350e
Bubalehich <janeridebmx@gmail.com>
2023-02-07 23:41:01 +0300

    refactor: update main class with countReader

--- src/main/java/ru/clevertec/Application.java
+++ src/main/java/ru/clevertec/Application.java
@@ -1,10 +1,13 @@
 package ru.clevertec;
 
+import ru.clevertec.util.CountReader;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        var count = CountReader.read();
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("Hello world!");
+        }
     }
 }
