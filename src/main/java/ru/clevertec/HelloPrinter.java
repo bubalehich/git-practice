b40a40ee17d7b55d7481061abf389341c0af57aa
Bubalehich <janeridebmx@gmail.com>
2023-02-07 14:10:39 +0300

    feat: add print method

--- src/main/java/ru/clevertec/HelloPrinter.java
+++ src/main/java/ru/clevertec/HelloPrinter.java
@@ -2,4 +2,9 @@ package ru.clevertec;
 
 public class HelloPrinter {
 
+public static void print(int printCount){
+    for (int i = 0; i< printCount; i++){
+        System.out.println("Hello world!");
+    }
+}
 }
