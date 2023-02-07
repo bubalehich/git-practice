690506a9ef5b90840caa031a7f5c06222d19ba85
Bubalehich <janeridebmx@gmail.com>
2023-02-07 23:46:20 +0300

    feat: add output text as a constant

--- src/main/java/ru/clevertec/Application.java
+++ src/main/java/ru/clevertec/Application.java
@@ -3,11 +3,13 @@ package ru.clevertec;
 import ru.clevertec.util.CountReader;
 
 public class Application {
+    private final static String outputText = "Hello world!";
+
     public static void main(String[] args) {
         var count = CountReader.read();
 
         for (int i = 0; i < count; i++) {
-            System.out.println("Hello world!");
+            System.out.println(outputText);
         }
     }
 }
