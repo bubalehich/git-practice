538e78685310bcb3eda7689f04fa2a7f89fd061d
Bubalehich <janeridebmx@gmail.com>
2023-02-07 23:46:56 +0300

    fix: fix sonar issues

--- src/main/java/ru/clevertec/Application.java
+++ src/main/java/ru/clevertec/Application.java
@@ -3,13 +3,13 @@ package ru.clevertec;
 import ru.clevertec.util.CountReader;
 
 public class Application {
-    private final static String outputText = "Hello world!";
+    private final static String OUTPUT_TEXT = "Hello world!";
 
     public static void main(String[] args) {
         var count = CountReader.read();
 
         for (int i = 0; i < count; i++) {
-            System.out.println(outputText);
+            System.out.println(OUTPUT_TEXT);
         }
     }
 }
